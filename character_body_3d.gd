dbe09acc7f9b5fed4a86564cf003c77f939d4ac5
peache2 <eaguilar@elpuig.xeill.net>
2025-10-03 22:53:51 +0200

    3

--- character_body_3d.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody3D
-
-
-@onready var spring_arm: SpringArm3D = $SpringArm3D
-
-func _ready():
-	# Evita que el springarm haga colisiones
-	spring_arm.add_excluded_object(self)
-	
