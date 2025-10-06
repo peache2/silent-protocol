563fe94076ae6bcf573253fc13b639f1c02bb373
«peache2» <eaguilar@elpuig.xeill.net>
2025-10-06 11:44:17 +0200

    .

--- meshes/character/scripts/principal_player.gd
+++ meshes/character/scripts/principal_player.gd
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("Walk_Left", "Walk_Right", "Walk_Forward", "Walk_Backward")
+	var input_dir := Input.get_vector("Walk_Right", "Walk_Left", "Walk_Backward", "Walk_Forward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
