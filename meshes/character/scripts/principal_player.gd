64971b50dfeef2738c0417332a3504c404917aa7
«peache2» <eaguilar@elpuig.xeill.net>
2025-10-08 13:32:30 +0200

    .

--- meshes/character/scripts/principal_player.gd
+++ meshes/character/scripts/principal_player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+
+const SPEED = 1.0
 const JUMP_VELOCITY = 4.5
 
 
@@ -21,3 +22,5 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	move_and_slide()	
+	
+	
